d3f1a4deebd25a919e9f23d238771a9e6e12ad8e
bnros <b.ros92@gmail.com>
2023-02-03 06:36:49 -0800

    completed final week 6 assessment, finally

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,7 +12,33 @@
 
 // --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
 
+// PSEUDOCODE:
+// input: an array of objects
+// output: a new array with a sentence about each person and their name capitlized
+
+//  process:
+// declare a function called "descPeople"
+// after declaring function, give it the paramater of "people" as an argument to take in an array of objects
+// create a return statement with using the .map method on the "people" array so that it will return a new array with the specifications we will give it
+// within the map statement, declare a variable called "fullName" and using the .split method, have it split into two seperate strings wuth a space in a space inside
+// declare two variable inside called "capFirstName" and "capLastName" to account for the two strings we have from using the .string method
+// in both of these new variables, call on the "fullName" variable and using bracket notation of [0][0] and [1][0], respectively. the first number which is 0, will access the first element in the name array, the second number which is 0, will then access the first character of that first element. the same will apply with the other variable.
+// use the .toUpperCase method following the bracket notation to have those letters accessed via bracket notation to capitlize
+// concat, then use the .slice method to extract all characters of the first and last name, except for the first one
+// using string interpolation, create a return statement to make your test pass
+
 // a) Create a test with an expect statement using the variable provided.
+
+// RED:
+describe("descPeople", () => {
+  it("takes an object and makes a sentence about them with their name capitlized", () => {
+    expect(descPeople(people)).toEqual([
+      "Ford Prefect is a hitchhiker.", 
+      "Zaphod Beeblebrox is president of the galaxy.", 
+      "Arthur Dent is a radio employee."
+    ])
+  })
+}) 
 
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
@@ -23,9 +49,78 @@ const people = [
 
 // b) Create the function that makes the test pass.
 
+//GREEN:
+
+// const descPeople = (persons) => {
+//   return persons.map(person => {
+//     const capName = person.name.charAt(0).toUpperCase() + person.name.slice(1)
+//     return `${capName} is ${person.occupation}.`
+//   })
+// }
+
+// ^Did not work, would only capitilize the first character of the first name in the array.
+
+// expect(received).toEqual(expected) // deep equality
+
+//     - Expected  - 3
+//     + Received  + 3
+
+//       Array [
+//     -   "Ford Prefect is a hitchhiker.",
+//     -   "Zaphod Beeblebrox is president of the galaxy.",
+//     -   "Arthur Dent is a radio employee.",
+//     +   "Ford prefect is a hitchhiker.",
+//     +   "Zaphod beeblebrox is president of the galaxy.",
+//     +   "Arthur dent is a radio employee.",
+
+// const descPeople = (persons) => {
+//   return persons.map(person => { 
+//     const name = person.name.split(" ")
+//     const capFirstName = person.capFirstName[0].toUpperCase() + person.capFirstName.slice(1)
+//     const capLastName = person.capLastName[0].toUpperCase() + person.capLastName.slice(1)
+//     return `${capFirstName} ${capLastName} is ${person.occupation}.`
+//   })
+// }
+
+// ^Also did not work
+
+const descPeople = (people)=> {
+  return people.map(person => {
+    let fullName = person.name.split(" ")
+    let capFirstName = fullName[0][0].toUpperCase() + fullName[0].slice(1)
+    let capLastName = fullName[1][0].toUpperCase() + fullName[1].slice(1)
+    return `${capFirstName} ${capLastName} is ${person.occupation}.`
+  })
+}
+
+// this one was really tough, referred to these resources for help:
+// https://javascript.plainenglish.io/javascript-capitalize-first-letter-of-each-word-in-array-79635ce43aac
+// https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
+
+
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
+// PSEUDOCODE:
+// input: an array of mixed data
+// output: array of remainders of numbers when divided by three
+
+// process:
+// declare a fxn called "divByThree"
+// after declaring fxn, pass it the paramater of array
+// within the new fxn, create a return statement to return our parameter first using the .filter method to create a new array containing only the specifications we give it, in this case, numbers
+// create a callback fxn to check if each value in the array is a number by using typeof and strictly setting it to the data type of "numbers"
+// use the .map method to take a callback function that creates an array from the results of the og array and give it a parameter called "num"
+// create a return statement, using the modulo that takes number and divides it by three
+
 // a) Create a test with an expect statement using the variables provided.
+
+// RED:
+describe("divByThree", () => {
+  it("returns only remainder of numbers divided by three", () => {
+    expect(divByThree(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(divByThree(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
 
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
@@ -34,9 +129,37 @@ const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 
 // b) Create the function that makes the test pass.
 
+const divByThree = (array) => {
+  return array.filter(function(value) {
+    return typeof value === 'number'
+      }).map(function(num) {
+    return num % 3
+  })
+}
+
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
+// pseudocode:
+// input: an array of numbers
+// output an array of said numbers, cubed
+
+// process:
+// start by declaring a function called "cubedNum"
+// after declaring the fxn, give it the paramater of "numsArray"
+// within the function, create a new variable called "cubedSum" and set to 0, this will be used to track the sum of numbers after it has been cubed in our array
+// create a for loop to iterate and cycle through each number in the array - have it to start at 0, then use the .length method to continue through the length of the array
+// in the loop, have the cubedSum add to the cubed number by using Math.pow
+// create a return statement for "cubedSum"
+
 // a) Create a test with an expect statement using the variables provided.
+
+// RED:
+describe("cubedNum", () => {
+  it("cubes all the numbers and adds the total sum", () => {
+    expect(cubedNum(cubeAndSum1)).toEqual(99)
+    expect(cubedNum(cubeAndSum2)).toEqual(1125)
+  })
+})
 
 const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
@@ -44,3 +167,13 @@ const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
 // b) Create the function that makes the test pass.
+
+// GREEN:
+
+const cubedNum = (numArray) => {
+  let cubedSum = 0
+  for (let i = 0; i < numArray.length; i++) {
+    cubedSum += Math.pow(numArray[i], 3)
+  }
+  return cubedSum
+}
--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,63 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: I am not sure how to go about fixing this mistake.
 
-Researched answer:
+Researched answer: If I forgot to add the foreign key, I can start fixing this mistake by generating a new migration to add the foreign key. Once this has been done, I can modify the migration file by adding data into your table. Once that is done, I should rub the migration, and update the new models to reflect the new associations with the names of the tables and columns. The foreign key will be named cohort_id, and will be in the student model.
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: The RESTful routes of Show, Edit, Destroy, and Update must always be passed params because they point to specific data called by a key id number.
 
-Researched answer:
+Researched answer: As listed above, asides from show, the RESTful routes of edit, destroy, and update are the ones that always needs to be passed params. This is due to the fact that with these three, they operate on very specific resources, and they need to know which resource needs to be acted on, which is indicated by the ID paramaeter. In short, generally speaking RESTful routes that modify (create, update, and destroy) will require parameters, whereas routes that retrieve (index and show) will not.
 
 3. Name three rails generator commands. What is created by each?
 
-Your answer:
+Your answer: Three rails generate commands are:
+1. $ rails generate model -- this command is what we've used most thus far, and it created a model as the name implies, and the associated migration file.
+2. $ rails generate controller -- also, as the name implies, this generate command will create a controller.
+3.  $ rails generate migration - this generate command will create a database migration file.
 
-Researched answer:
+Researched answer: Upon doing additional research, I learned that there are other additional rails generator commands that are even more useful! In addition to the ones I listed above, I learned that there is also a $ rails generate scaffold command - this command does a lot of the heavy lifting and creates full CRUD resources, and also the model, controller, views, and migrations. Can be extremely useful in upcoming projects! Besides this, there is also $ rails generate mailer, which creates a mailer (enables apps to send and receive emails)
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
 action: "GET" location: /students
 
+The action above will use "index". This will display all the students in the db.
+
 action: "POST" location: /students
+
+The action above will use "create". This will allow a new student to be added,
 
 action: "GET" location: /students/new
 
+The action above will use "new". THis will display a form for new student data to be inputted.
+
 action: "GET" location: /students/2
+
+The action above will use "show". This will display information corresponding to the student with the id number of 2.
 
 action: "GET" location: /students/2/edit
 
+The actionn above will use "edit". This will display a form to update information for the student with the id number of 2.
+
 action: "PATCH" location: /students/2
 
+The action above will use "update". This will take the new, updated student information above, and replace the existing student data of the student with the key id number of 2.
+
 action: "DELETE" location: /students/2
+The action above will use "destroy". This will delete student data information of the student with the key id number of 2 out of the db.
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+1. As a user, I want to be able to see all my tasks on my to do list.
+2. As a user, I want to be able to mark my tasks as complete and update it.
+3. As a user, I want to be able to add a new task onto my to-do list
+4. As a user, I want to be able to edit existing tasks.
+5. As a user, I want to be able to remove a task.
+6. As a user, I want to be able to be able to organize my tasks by date and priority.
+7. As a user, I want to be able to create additional to-do list for tasks.
+8. As a user I want to be able to track progress.
+9. As a user I want to able to add notes about the task.
+10. As a user, I want to see a user friendly interface.
